92cf4a882c5dbada3d880592bcb04b63fb1a5a05
Whitney Hiemstra <whitcolorado@gmail.com>
2015-07-09 14:02:51 -0600

    Able to edit restaurant details, working on creating an item for that restaurant

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,37 @@
 class ItemsController < ApplicationController
+  before_action :load_restaurant, only: [:new, :create]
 
   def index
   end
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    # binding.pry
+    item = Item.new(item_params)
+    item.restaurant_id = @restaurant.id
+
+    if item.save
+      redirect_to @restaurant
+    else
+      flash.now[:errors] = item.errors.full_messages.join(", ")
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    binding.pry
+    params.require(:item).permit(:name, :description, :price)
+  end
+
+  def load_restaurant
+    @restaurant = Restaurant.find_by(display_name: params[:restaurant_id])
   end
 end
