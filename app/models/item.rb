582fb129560ea8d5132672a4e577d33a1a29d320
Whitney Hiemstra <whitcolorado@gmail.com>
2015-07-15 12:32:41 -0600

    WIP - front end styling

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,8 +18,8 @@ class Item < ActiveRecord::Base
   has_attached_file :image, :default_url => ':placeholder'
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  # has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  # validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   default_scope { order('name ASC')}
 
