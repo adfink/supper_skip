6359f9bdfc41e4793cad79f677b979831b66d24c
Andrew Fink <andrewfink8@gmail.com>
2015-07-14 19:58:07 -0600

    mailers WIP

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,7 @@
 class NotificationMailer < ApplicationMailer   #ActionMailer::Base
   default from: "from@example.com"
   def staff_registration_email(email_address)
-    binding.pry
+    # binding.pry
     mail(to: email_address, subject: "You're hired!")
   end
 end
