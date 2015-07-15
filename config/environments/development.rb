9ef8e6db1ac5ced78ae895440fb34738a9ad76c4
Andrew Fink <andrewfink8@gmail.com>
2015-07-14 20:42:16 -0600

    mail works ... yay

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,8 +34,9 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'smtp.mandrillapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  # host = 'smtp.mandrillapp.com'
+  # config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: "localhost:3000"}
 
 
 
